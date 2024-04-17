32cb35bbe1c9f45fb83a9d2e250cbfeb36e8f697
Niteshkumar2004 <167275927+Niteshkumar2004@users.noreply.github.com>
2024-04-17 10:15:12 +0530

    Create storekeys.py

---
+++ storekeys.py
@@ -0,0 +1,14 @@
+ store = dict()
+print(store)
+store['p']= 23
+store['m']= 12
+store['#']= 60
+store['hello']= 222
+print(store)
+
+allkeys = store.keys()
+print(allkeys)
+
+
+for ele in allkeys:
+    print(ele)
