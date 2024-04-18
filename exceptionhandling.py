55ddddd5ab35630fb9ec8b22983a789ddace896c
Niteshkumar2004 <167275927+Niteshkumar2004@users.noreply.github.com>
2024-04-18 10:11:40 +0530

    Create exceptionhandling.py

---
+++ exceptionhandling.py
@@ -0,0 +1,14 @@
+print("starting line")
+a=[11,22,33]
+try:
+    a = 10 / 5
+    print(y)
+except:
+    print("some exception raised")
+else:
+    print("no exception raised, everything worked perfectly")
+
+finally:
+    print("this is a final block ")
+    print("this is a block")
+print("outside try block")
